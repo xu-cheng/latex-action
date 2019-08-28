241eb04eebdf265650d4d207eea8d80e9c9a19aa
Cheng XU <git@xuc.me>
2019-08-28 18:04:05 +0800

    biber/biblatex 3.13 are released

--- setup.sh
+++ setup.sh
@@ -20,23 +20,10 @@ tar --strip-components 1 -zxf /tmp/install-tl/install-tl-unx.tar.gz \
 echo "==> Install Packages"
 tlmgr update --self
 tlmgr install \
+  biber \
+  biblatex \
   latexmk \
   texliveonfly
-
-# install biber/biblatex from source
-# issue: https://github.com/plk/biber/issues/255
-mkdir -p /tmp/install-biber
-wget -nv https://downloads.sourceforge.net/project/biblatex-biber/biblatex-biber/development/binaries/Linux-musl/biber-linux_x86_64-musl.tar.gz \
-  -O /tmp/install-biber/biber-linux_x86_64-musl.tar.gz
-tar -zxf /tmp/install-biber/biber-linux_x86_64-musl.tar.gz -C /tmp/install-biber
-mv /tmp/install-biber/biber-linux_x86_64-musl /opt/texlive/texdir/bin/x86_64-linuxmusl/biber
-wget -nv https://downloads.sourceforge.net/project/biblatex/development/biblatex-3.13.tgz \
-  -O /tmp/install-biber/biblatex-3.13.tgz
-tar -zxf /tmp/install-biber/biblatex-3.13.tgz -C /tmp/install-biber
-mv /tmp/install-biber/biblatex/latex /opt/texlive/texmf-local/tex/latex/biblatex
-mv /tmp/install-biber/biblatex/bibtex/bst /opt/texlive/texmf-local/bibtex/bst/biblatex
-mv /tmp/install-biber/biblatex/bibtex/bib /opt/texlive/texmf-local/bibtex/bib/biblatex
-texhash
 
 echo "==> Clean up"
 rm -rf \
