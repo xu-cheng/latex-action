92613a68fad8d5120d3fbd4b6ac76da6e9f45498
Luke Pereira <lukejoepereira@gmail.com>
2020-04-23 05:33:37 -0400

    Modified action to compile muliple latex docs using changed_files arg

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Modified action to compile multiple files from a list of modified .tex files specified in changed_files arg.
+
 # latex-action
 
 [![GitHub Actions Status](https://github.com/xu-cheng/latex-action/workflows/Test%20Github%20Action/badge.svg)](https://github.com/xu-cheng/latex-action/actions)
--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,10 @@ compiler="$3"
 args="$4"
 extra_packages="$5"
 extra_system_packages="$6"
+changed_files="$7"
 
-if [ -z "$root_file" ]; then
-  error "Input 'root_file' is missing."
+if [ -z "$root_file" ] || [-z "$changed_files"]; then
+  error "Input 'root_file' or 'changed_files' is missing."
 fi
 
 if [ -z "$compiler" ] && [ -z "$args" ]; then
@@ -47,5 +48,19 @@ if [ ! -f "$root_file" ]; then
   error "File '$root_file' cannot be found from the directory '$PWD'."
 fi
 
+if [ -n "$working_directory" ]; then
+  cd "$working_directory"
+fi
+
 # shellcheck disable=SC2086
-"$compiler" $args "$root_file"
+if [-n "$root_file"]; 
+  then
+    "$compiler" $args "$root_file"
+  else
+    for i in `cat $changed_files`; do 
+      cd "$(dirname "$i")";
+      "$compiler" $args "$(basename $i)"; 
+      cd -
+    done
+fi
+
